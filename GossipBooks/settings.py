cc7b966ad30e37a49d1400779a7eb8287c142753
sabiprogrammer <obedisonline@gmail.com>
2021-01-17 18:28:57 +0100

    switched to local db for heroku

--- GossipBooks/settings.py
+++ GossipBooks/settings.py
@@ -101,23 +101,23 @@ WSGI_APPLICATION = 'GossipBooks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd9t0iccvmsl35s',
-        'HOST': 'ec2-23-20-205-19.compute-1.amazonaws.com',
-        'PORT': 5432,
-        'USER': 'wnducfffnqafij',
-        'PASSWORD': '97dc744a5b901e34228171b4955eddb4e4417497c96c77cf7792f7208a9220cc'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'd9t0iccvmsl35s',
+#         'HOST': 'ec2-23-20-205-19.compute-1.amazonaws.com',
+#         'PORT': 5432,
+#         'USER': 'wnducfffnqafij',
+#         'PASSWORD': '97dc744a5b901e34228171b4955eddb4e4417497c96c77cf7792f7208a9220cc'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
